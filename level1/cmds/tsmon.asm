c26e6e63fde5f5be24520b48c49cdb1748983185
boisy <unknown>
2002-07-14 04:48:22 +0000

    Migrated OS-9 Level Two command

--- level1/cmds/tsmon.asm
+++ level1/cmds/tsmon.asm
@@ -1,16 +1,16 @@
-********************************************************************
+*******************************************************************
 * Tsmon - Timesharing monitor
 *
 * $Id$
 *
 * Ed.    Comments                                       Who YY/MM/DD
 * ------------------------------------------------------------------
-*  7     Original Tandy/Microware version
+*  8     Original Tandy/Microware version
 
          nam   Tsmon
          ttl   Timesharing monitor
 
-* Disassembled 02/07/06 23:17:42 by Disasm v1.6 (C) 1988 by RML
+* Disassembled 02/07/13 23:44:55 by Disasm v1.6 (C) 1988 by RML
 
          ifp1
          use   defsfile
@@ -24,11 +24,12 @@ rev      set   $01
 
 u0000    rmb   1
 u0001    rmb   2
-u0003    rmb   452
+u0003    rmb   2
+u0005    rmb   451
 size     equ   .
 
 name     fcs   /Tsmon/
-         fcb   $07 
+         fcb   $08 
 
 L0013    fcc   "LOGIN"
 L0018    fcb   C$CR
@@ -37,50 +38,47 @@ L0019    rti
 
 start    stx   <u0001
          std   <u0003
-L001E    cmpd  #$0002
-         bcs   L0047
+         leax  <L0019,pcr
+         os9   F$Icpt   
+L0024    ldx   <u0001
+         ldd   <u0003
+         cmpd  #$0002
+         bcs   L0052
          lda   ,x
          cmpa  #C$CR
-         beq   L0047
+         beq   L0052
          clra  
-         os9   I$Close  
-         inca  
-         os9   I$Close  
-         inca  
          os9   I$Close  
          lda   #UPDAT.
          os9   I$Open   
-         bcs   L0082
+         bcs   L007B
+         inca  
+         os9   I$Close  
+         inca  
+         os9   I$Close  
+         clra  
          os9   I$Dup    
-         bcs   L0082
+         bcs   L007B
          os9   I$Dup    
-         bcs   L0082
-L0047    leax  <L0019,pcr
-         os9   F$Icpt   
-L004D    clra  
-         leax  ,-s
+         bcs   L007B
+L0052    clra  
+         leax  u0005,u
          ldy   #$0001
          os9   I$ReadLn 
-         leas  $01,s
-         bcc   L0061
-         cmpb  #E$HangUp
-         bne   L004D
-         bra   L007C
-L0061    lda   #$01
+         bcs   L0024
+         lda   #$01
          clrb  
          leax  <L0013,pcr
          leau  <L0018,pcr
          ldy   #$0000
          os9   F$Fork   
-         bcs   L004D
+         bcs   L0024
          sta   <u0000
-L0075    os9   F$Wait   
+L0072    os9   F$Wait   
          cmpa  <u0000
-         bne   L0075
-L007C    ldx   <u0001
-         ldd   <u0003
-         bra   L001E
-L0082    os9   F$Exit   
+         bne   L0072
+         bra   L0024
+L007B    os9   F$Exit   
 
          emod
 eom      equ   *
