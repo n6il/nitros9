6d022629afcc35b3398fdf8751579ebdd56178d3
boisy <unknown>
2002-07-20 04:15:07 +0000

    Cleaned up descriptor

--- level1/modules/p1_sc6551dragon.asm
+++ level1/modules/p1_sc6551dragon.asm
@@ -1,13 +1,11 @@
 ********************************************************************
-* progname - program module
+* P1 - Serial port used as printer device descriptor
 *
 * $Id$
 *
 * Ed.    Comments                                       Who YY/MM/DD
 * ------------------------------------------------------------------
 *  -     Original Dragon Data distribution version
-*
-*
 
          nam   P1
          ttl   Serial port used as printer device descriptor
@@ -16,16 +14,20 @@
 
          ifp1
          use   defsfile
+         use   scfdefs
          endc
+
 tylg     set   Devic+Objct   
 atrv     set   ReEnt+rev
 rev      set   $01
+
          mod   eom,name,tylg,atrv,mgrnam,drvnam
-         fcb   $03 mode byte
-         fcb   $FF extended controller address
+
+         fcb   READ.+WRITE. mode byte
+         fcb   HW.Page extended controller address
          fdb   $FF04  physical controller address
          fcb   initsize-*-1  initilization table size
-         fcb   $00 device type:0=scf,1=rbf,2=pipe,3=scf
+         fcb   DT.SCF device type:0=scf,1=rbf,2=pipe,3=scf
          fcb   $00 case:0=up&lower,1=upper only
          fcb   $00 backspace:0=bsp,1=bsp then sp & bsp
          fcb   $00 delete:0=bsp over line,1=return
@@ -33,7 +35,7 @@ rev      set   $01
          fcb   $01 auto line feed:0=off
          fcb   $00 end of line null count
          fcb   $00 pause:0=no end of page pause
-         fcb   $42 lines per page
+         fcb   66 lines per page
          fcb   $00 backspace character
          fcb   $00 delete line character
          fcb   $00 end of record character
@@ -46,16 +48,16 @@ rev      set   $01
          fcb   $00 backspace echo character
          fcb   $00 line overflow character (bell)
          fcb   $00 init value for dev ctl reg
-         fcb   $03 baud rate
+         fcb   B1200 baud rate
          fdb   name copy of descriptor name address
-         fcb   $11 acia xon char
-         fcb   $13 acia xoff char
+         fcb   C$XON acia xon char
+         fcb   C$XOFF acia xoff char
 initsize equ   *
-name     equ   *
-         fcs   /P1/
-mgrnam   equ   *
-         fcs   /scf/
-drvnam   equ   *
-         fcs   /acia51/
+
+name     fcs   /P1/
+mgrnam   fcs   /scf/
+drvnam   fcs   /acia51/
+
          emod
 eom      equ   *
+         end
