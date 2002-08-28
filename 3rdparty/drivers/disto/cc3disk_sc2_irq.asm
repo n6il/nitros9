ef68d75620d702a4516af2ed7306cbc9d8495d68
tlindner <unknown>
2002-08-28 04:57:20 +0000

    Added IRQ and sleep versions of Disto's no halt floppy controller's drvier.

--- 3rdparty/drivers/disto/cc3disk_sc2_irq.asm
+++ 3rdparty/drivers/disto/cc3disk_sc2_irq.asm
@@ -1,9 +1,9 @@
 ********************************************************************
 * CC3Disk - Disto No Halt Super Controller II disk driver
 *
-* This driver their 'sleep' driver. It is a no halt (multitasking)
-* disk driver, but it does not use interrupts. Thus it is safe to use
-* this driver with other hardware uses interrupts.
+* This driver their interrupt driven driver. It is a no halt
+* (multitasking) disk driver that uses interrupts. Care must be
+* taken that no other hardware will conflict.
 *
 * $Id$
 *
@@ -14,7 +14,7 @@
          nam   CC3Disk
          ttl   os9 device driver    
 
-* Disassembled 02/08/27 11:42:59 by Disasm v1.6 (C) 1988 by RML
+* Disassembled 02/08/27 11:42:37 by Disasm v1.6 (C) 1988 by RML
 
 * Disto's Super Controller II supports two locations for its
 * registers: $FF74 and $FF58
@@ -24,7 +24,7 @@ nh_stat  equ   nh_base
 nh_data  equ   nh_base+2
 
          ifp1
-         use   defsfile
+         use   os9defs
          endc
 tylg     set   Drivr+Objct   
 atrv     set   ReEnt+rev
@@ -46,299 +46,354 @@ u0012    rmb   1
 u0013    rmb   31
 u0032    rmb   24
 u004A    rmb   6
-u0050    rmb   47
-u007F    rmb   11
-u008A    rmb   28
-u00A6    rmb   1
-u00A7    rmb   10
-u00B1    rmb   13
+u0050    rmb   48
+u0080    rmb   10
+u008A    rmb   8
+u0092    rmb   16
+u00A2    rmb   5
+u00A7    rmb   8
+u00AF    rmb   2
+u00B1    rmb   11
+u00BC    rmb   1
+u00BD    rmb   1
 size     equ   .
          fcb   $FF 
 name     equ   *
          fcs   /CC3Disk/
-         fcb   $A2 "
+         fcb   $A3 #
 start    equ   *
-         lbra  L003C
-         lbra  L0084
-         lbra  L010E
-         lbra  L0081
-         lbra  L030F
+         lbra  L0046
+         lbra  L00BF
+         lbra  L013B
+         lbra  L00BC
+         lbra  L0370
          leay  >u00B1,u
          ldx   #$0000
          os9   F$VIRQ   
+         os9   F$IRQ    
+         ldy   #nh_data
          os9   F$IRQ    
          clrb  
          stb   >$FF40
          stb   <u0032
          rts   
-L0039    fcb   $00
+L0040    fcb   $00
          fcb   $01
          fcb   $09
-L003C    clr   >nh_data
+L0043    fcb   $80     suba #$80  This doesn't make sense.
+         fcb   $80
+         fcb   $10     fcb $10
+L0046    clr   >nh_data
          clr   <u0032
          ldx   #$FF48
          lda   #$D0
          sta   ,x
-         lbsr  L02D9
+         lbsr  L033A
          lda   ,x
          lda   #$FF
          sta   >u00A7,u
          ldb   #$04
          leax  u000F,u
-L0057    sta   ,x
+L0061    sta   ,x
          sta   <$15,x
          leax  <$26,x
          decb  
-         bne   L0057
-         leax  >L01C0,pcr
-         stx   <D.NMI
+         bne   L0061
+         leax  >L023C,pcr
+         stx   $FC
          pshs  u
          leau  >u00A7,u
          leay  u000E,u
          tfr   y,d
-         leay  >L0428,pcr
-         leax  >L0039,pcr
+         leay  >L0489,pcr
+         leax  >L0040,pcr
          os9   F$IRQ    
          puls  u
-         bcs   L0082
-L0081    clrb  
-L0082    rts   
-L0083    rts   
-L0084    lbsr  L01E7
+         bcs   L00BD
+         lda   >$FF7F
+         sta   >u00BC,u
+         ldd   #nh_data
+         leay  >L01F7,pcr
+         leax  >L0043,pcr
+         os9   F$IRQ    
+         bcs   L00BD
+         pshs  cc
+         orcc  #$50
+         lda   >$FF23
+         anda  #$FC
+         sta   >$FF23
+         lda   >$FF22
+         lda   <u0092
+         ora   #$01
+         sta   <u0092
+         sta   >$FF92
+         puls  cc
+L00BC    clrb  
+L00BD    rts   
+L00BE    rts   
+L00BF    lbsr  L0263
          clr   u0003,u
          ldd   <u0010,u
-         bne   L00D8
-         bsr   L00D8
-         bcs   L0083
+         bne   L0113
+         bsr   L0113
+         bcs   L00BE
          lda   <$23,y
          bita  #$40
-         lbne  L043B
+         lbne  L04B0
          ldx   $08,y
          pshs  y,x
          ldy   <$1E,y
          ldb   #$14
-L00A5    lda   b,x
+L00E0    lda   b,x
          sta   b,y
          decb  
-         bpl   L00A5
+         bpl   L00E0
          lda   <$10,y
          ldy   $02,s
          ldb   <$24,y
          bita  #$02
-         beq   L00BD
+         beq   L00F8
          bitb  #$01
-         beq   L00D3
-L00BD    bita  #$04
-         beq   L00C5
+         beq   L010E
+L00F8    bita  #$04
+         beq   L0100
          bitb  #$02
-         beq   L00D3
-L00C5    bita  #$01
-         beq   L00D0
+         beq   L010E
+L0100    bita  #$01
+         beq   L010B
          lda   <$27,y
          suba  #$02
-         bcs   L00D3
-L00D0    clrb  
+         bcs   L010E
+L010B    clrb  
          puls  pc,y,x
-L00D3    comb  
+L010E    comb  
          ldb   #$F9
          puls  pc,y,x
-L00D8    lbsr  L0257
-         bcs   L0083
+L0113    lbsr  L02D3
+         bcs   L00BE
          ldb   #$80
          lda   #$07
-         lbsr  L016C
-         bcc   L00F6
-         ldb   >$FF48
-         clr   >nh_data
-         lda   u0001,u
-         ora   #$08
-         sta   >$FF40
-         lbra  L01E3
-L00F6    ldx   $08,y
+         lbsr  L0199
+         lbcs  L025F
+         ldx   $08,y
          ldb   #$80
          tst   u0003,u
-         bne   L010B
+         bne   L0138
          pshs  b
-L0100    ldd   >nh_stat
+L012D    ldd   >nh_stat
          std   ,x++
          dec   ,s
-         bne   L0100
+         bne   L012D
          puls  b
-L010B    andcc #$FE
+L0138    andcc #$FE
          rts   
-L010E    lbsr  L01E7
-L0111    bsr   L0120
-         bcs   L011F
+L013B    lbsr  L0263
+L013E    bsr   L014D
+         bcs   L014C
          tst   <$28,y
-         bne   L011E
-         bsr   L0144
-         bcs   L0111
-L011E    clrb  
-L011F    rts   
-L0120    lbsr  L0257
-         bcs   L011F
+         bne   L014B
+         bsr   L0171
+         bcs   L013E
+L014B    clrb  
+L014C    rts   
+L014D    lbsr  L02D3
+         bcs   L014C
          ldx   $08,y
          lda   #$04
          sta   >nh_data
          ldb   #$80
          pshs  b
-L0130    ldd   ,x++
+L015D    ldd   ,x++
          std   >nh_stat
          dec   ,s
-         bne   L0130
+         bne   L015D
          puls  b
          ldb   #$A0
          lda   #$06
-         bsr   L016C
-         lbra  L01C4
-L0144    lda   u0004,u
+         bsr   L0199
+         lbra  L0240
+L0171    lda   u0004,u
          pshs  a
          clr   u0004,u
          lda   #$FF
          sta   u0003,u
-         lbsr  L00D8
-         bcs   L0167
+         lbsr  L0113
+         bcs   L0194
          pshs  b
-L0155    ldd   >nh_stat
+L0182    ldd   >nh_stat
          cmpd  ,x++
-         bne   L0163
+         bne   L0190
          dec   ,s
-         bne   L0155
-         bra   L0165
-L0163    orcc  #$01
-L0165    puls  b
-L0167    puls  a
+         bne   L0182
+         bra   L0192
+L0190    orcc  #$01
+L0192    puls  b
+L0194    puls  a
          sta   u0004,u
          rts   
-L016C    std   u0008,u
-L016E    ldd   u0008,u
-         bsr   L018D
-         bcs   L0179
-         lbsr  L01C4
-         bcc   L018C
-L0179    lda   >$FF48
+L0199    std   u0008,u
+L019B    ldd   u0008,u
+         bsr   L01B8
+         lbsr  L0240
+         bcc   L01B7
+         lda   >$FF48
          bita  #$40
-         bne   L018B
+         bne   L01B6
          lsr   u0004,u
-         beq   L018B
-         bcc   L016E
-         lbsr  L02E5
-         bra   L016E
-L018B    coma  
-L018C    rts   
-L018D    stb   >$FF48
+         beq   L01B6
+         bcc   L019B
+         lbsr  L0346
+         bra   L019B
+L01B6    coma  
+L01B7    rts   
+L01B8    pshs  a
+         lda   <u0050
+         sta   >-u00A2,u
+         puls  a
+         stb   >$FF48
+         ora   #$08
          sta   >nh_data
          ldb   #$28
          orb   u0001,u
          stb   >$FF40
-         ldb   #$04
-         lda   #$FF
-         pshs  x,a
-L01A0    ldx   #$0001
-         lbsr  L03C3
-         dec   ,s
-         beq   L01B5
-         tst   >nh_data
-         bmi   L01A0
-         stb   >nh_data
+         pshs  x
+         bra   L01E5
+L01D5    ldx   <u0050
+         lda   $0C,x
+         ora   #$08
+         sta   $0C,x
+         andcc #$AF
+         ldx   #$0001
+         lbsr  L0424
+L01E5    orcc  #$50
+         lda   >-u00A2,u
+         bne   L01D5
          clrb  
-         puls  pc,x,a
-L01B5    stb   >nh_data
-         lda   #$D0
-         sta   >$FF48
-         comb  
-         puls  pc,x,a
-L01C0    leas  $0C,s
+         ldb   #$04
+         stb   >nh_data
+         andcc #$AF
+         puls  pc,x
+L01F7    lda   u0005,u
+         beq   L0233
+         ldb   >$FF7F
+         stb   >u00BD,u
+         ldb   >u00BC,u
+         stb   >$FF7F
+         ldb   #$D0
+         stb   >$FF48
+         ldb   #$04
+         stb   >nh_data
+         ldb   <u00AF
+         andb  #$FE
+         stb   <u00AF
+         ldb   <u0092
+         andb  #$FE
+         stb   >$FF92
+         orb   #$01
+         stb   >$FF92
+         clrb  
+         stb   u0005,u
+         tfr   d,x
+         lda   $0C,x
+         anda  #$F7
+         sta   $0C,x
+         clrb  
+         bra   L0234
+L0233    comb  
+L0234    lda   >u00BD,u
+         sta   >$FF7F
+         rts   
+L023C    leas  $0C,s
          puls  y,cc
-L01C4    ldb   >$FF48
+L0240    ldb   >$FF48
          clr   >nh_data
          andb  #$F8
-         beq   L01DC
+         beq   L0258
          pshs  x
-         leax  <L01DD,pcr
-L01D3    leax  $01,x
+         leax  <L0259,pcr
+L024F    leax  $01,x
          rolb  
-         bcc   L01D3
+         bcc   L024F
          ldb   ,x
          puls  pc,x
-L01DC    clrb  
-L01DD    rts   
+L0258    clrb  
+L0259    rts   
          fcb   $F6
          fcb   $F2
          fcb   $F5
          fcb   $F7
          fcb   $F3
-L01E3    comb  
+L025F    comb  
          ldb   #$F4
          rts   
-L01E7    leau  >u00A7,u
+L0263    leau  >u00A7,u
          clr   u0007,u
          lda   #$91
          sta   u0004,u
          tstb  
-         bne   L0203
+         bne   L027F
          tfr   x,d
          std   <u0010,u
-         beq   L0225
+         beq   L02A1
          ldx   <$1E,y
          cmpd  $01,x
-         bcs   L0209
-L0203    comb  
+         bcs   L0285
+L027F    comb  
          ldb   #$F1
          leas  $02,s
          rts   
-L0209    clr   ,-s
-         bra   L020F
-L020D    inc   ,s
-L020F    subd  <$11,x
-         bcc   L020D
+L0285    clr   ,-s
+         bra   L028B
+L0289    inc   ,s
+L028B    subd  <$11,x
+         bcc   L0289
          addd  <$11,x
          lda   <$10,x
          lsra  
-         bcc   L0223
+         bcc   L029F
          lsr   ,s
-         bcc   L0223
+         bcc   L029F
          inc   u0007,u
-L0223    puls  a
-L0225    std   u0005,u
+L029F    puls  a
+L02A1    std   u0005,u
          clrb  
          rts   
-L0229    clr   u0002,u
+L02A5    clr   u0002,u
          lda   <$21,y
          cmpa  #$04
-         bcs   L0236
+         bcs   L02B2
          comb  
          ldb   #$F0
          rts   
-L0236    pshs  x,b,a
+L02B2    pshs  x,b,a
          cmpa  ,u
-         beq   L023E
+         beq   L02BA
          com   u0002,u
-L023E    sta   ,u
-         leax  <L024C,pcr
+L02BA    sta   ,u
+         leax  <L02C8,pcr
          ldb   a,x
          stb   u0001,u
-         lbsr  L03DD
+         lbsr  L043E
          puls  pc,x,b,a
-L024C    oim   #$02,<u0004
+L02C8    oim   #$02,<u0004
          nega  
-L0250    pshs  a
+L02CC    pshs  a
          ldb   <$15,x
-         bra   L028E
-L0257    lbsr  L0229
-         bcs   L02B1
+         bra   L030A
+L02D3    lbsr  L02A5
+         bcs   L032D
          ldd   u0005,u
          pshs  a
          lda   u0007,u
-         beq   L026A
+         beq   L02E6
          lda   u0001,u
          ora   #$40
          sta   u0001,u
-L026A    lda   <$23,y
+L02E6    lda   <$23,y
          bita  #$02
-         bne   L0272
+         bne   L02EE
          incb  
-L0272    stb   >$FF4A
+L02EE    stb   >$FF4A
          ldx   <$1E,y
          ldb   <$15,x
          lda   <$10,x
@@ -348,117 +403,105 @@ L0272    stb   >$FF4A
          pshs  a
          lda   $01,s
          tst   ,s+
-         beq   L028E
+         beq   L030A
          lsla  
          lslb  
-L028E    stb   >$FF49
+L030A    stb   >$FF49
          tst   u0002,u
-         bne   L029C
+         bne   L0318
          ldb   ,s
          cmpb  <$15,x
-         beq   L02A8
-L029C    sta   >$FF4B
+         beq   L0324
+L0318    sta   >$FF4B
          ldb   <$22,y
          andb  #$03
          eorb  #$1B
-         bsr   L02B2
-L02A8    puls  a
+         bsr   L032E
+L0324    puls  a
          sta   <$15,x
          sta   >$FF49
          clrb  
-L02B1    rts   
-L02B2    bsr   L02D7
-L02B4    ldb   >$FF48
-         bitb  #$01
-         beq   L02D9
-         ldd   #$00F0
-         std   u000A,u
-         pshs  x
-         ldx   #$0001
-         lbsr  L03C3
-         puls  x
-         bra   L02B4
-L02CC    lda   #$08
-         ora   u0001,u
-         sta   >$FF40
-         stb   >$FF48
+L032D    rts   
+L032E    lda   #$04
+         lbsr  L01B8
+         lda   >$FF48
+         clr   >nh_data
          rts   
-L02D7    bsr   L02CC
-L02D9    clr   <u0012,u
+L033A    clr   <u0012,u
          inc   <u0012,u
-L02DF    rol   <u0012,u
-         bpl   L02DF
+L0340    rol   <u0012,u
+         bpl   L0340
          rts   
-L02E5    pshs  x,b
-         lbsr  L0229
-         bcs   L030D
+L0346    pshs  x,b
+         lbsr  L02A5
+         bcs   L036E
          ldx   <$1E,y
          clr   <$15,x
          lda   #$04
-L02F4    ldb   <$22,y
+L0355    ldb   <$22,y
          andb  #$03
          eorb  #$4B
          pshs  a
-         bsr   L02B2
+         bsr   L032E
          puls  a
          deca  
-         bne   L02F4
+         bne   L0355
          ldb   <$22,y
          andb  #$03
          eorb  #$0B
-         bsr   L02B2
-L030D    puls  pc,x,b
-L030F    leau  >u00A7,u
+         bsr   L032E
+L036E    puls  pc,x,b
+L0370    leau  >u00A7,u
          ldx   $06,y
          ldb   $02,x
          cmpb  #$04
-         beq   L0323
+         beq   L0384
          cmpb  #$03
-         beq   L02E5
+         beq   L0346
          comb  
          ldb   #$D0
          rts   
-L0323    pshs  u,y
+L0384    pshs  u,y
          ldd   #$1A00
          os9   F$SRqMem 
-         lbcs  L0381
+         lbcs  L03E2
          ldx   $02,s
          stu   <$13,x
          ldx   <u0050
          lda   $06,x
-         ldb   <L00D0
+         ldb   $D0
          ldy   ,s
          ldx   $06,y
          ldx   $04,x
          ldy   #$1A00
          os9   F$Move   
-         bcs   L0372
+         bcs   L03D3
          puls  u,y
          pshs  u,y
-         lbsr  L0229
-         bcs   L0372
+         lbsr  L02A5
+         bcs   L03D3
          ldx   $06,y
          ldb   $07,x
          bitb  #$01
-         beq   L0363
+         beq   L03C4
          lda   u0001,u
          ora   #$40
          sta   u0001,u
          sta   u0007,u
-L0363    lda   $09,x
+L03C4    lda   $09,x
          ldx   <$1E,y
-         lbsr  L0250
-         bcs   L0372
+         lbsr  L02CC
+         bcs   L03D3
          ldx   <u0013,u
-         bsr   L0383
-L0372    ldu   $02,s
+         bsr   L03E4
+L03D3    ldu   $02,s
          pshs  b,cc
          ldu   <u0013,u
          ldd   #$1A00
          os9   F$SRtMem 
          puls  b,cc
-L0381    puls  pc,u,y
-L0383    pshs  y,cc
+L03E2    puls  pc,u,y
+L03E4    pshs  y,cc
          orcc  #$50
          ldb   #$F0
          stb   >$FF48
@@ -468,11 +511,11 @@ L0383    pshs  y,cc
          stb   >$FF40
          orb   #$A8
          lda   #$02
-         lbsr  L02D9
-L039E    bita  >$FF48
-         bne   L03B9
+         lbsr  L033A
+L03FF    bita  >$FF48
+         bne   L041A
          leay  -$01,y
-         bne   L039E
+         bne   L03FF
          lda   u0001,u
          ora   #$08
          sta   >$FF40
@@ -482,25 +525,25 @@ L039E    bita  >$FF48
          comb  
          ldb   #$F5
          rts   
-L03B9    lda   ,x+
+L041A    lda   ,x+
          sta   >$FF4B
          stb   >$FF40
-         bra   L03B9
-L03C3    pshs  b,a
+         bra   L041A
+L0424    pshs  b,a
          ldd   <u0050
          cmpd  <u004A
          puls  b,a
-         beq   L03D2
+         beq   L0433
          os9   F$Sleep  
          rts   
-L03D2    ldx   #$A000
-L03D5    nop   
+L0433    ldx   #$A000
+L0436    nop   
          nop   
          nop   
          leax  -$01,x
-         bne   L03D5
+         bne   L0436
          rts   
-L03DD    pshs  y,x,b,a
+L043E    pshs  y,x,b,a
          ldd   #$00F0
          std   u000A,u
          lda   u0001,u
@@ -508,47 +551,55 @@ L03DD    pshs  y,x,b,a
          sta   >$FF40
          ldx   #$0028
          lda   <u0032
-         bmi   L040A
-         beq   L0408
+         bmi   L046B
+         beq   L0469
          tst   u0002,u
-         beq   L040C
+         beq   L046D
          lda   <$23,y
          bita  #$10
-         beq   L040C
-         bsr   L03C3
+         beq   L046D
+         bsr   L0424
          ldd   #$00F0
          std   u000A,u
-         bra   L040C
-L0408    bsr   L03C3
-L040A    bsr   L040F
-L040C    clrb  
+         bra   L046D
+L0469    bsr   L0424
+L046B    bsr   L0470
+L046D    clrb  
          puls  pc,y,x,b,a
-L040F    lda   #$01
+L0470    lda   #$01
          sta   <u0032
          ldx   #$0001
          leay  u000A,u
          clr   $04,y
          ldd   #$00F0
          os9   F$VIRQ   
-         bcc   L0426
+         bcc   L0487
          lda   #$80
          sta   <u0032
-L0426    clra  
+L0487    clra  
          rts   
-L0428    pshs  a
-         lda   <u008A
-         beq   L0432
-         bsr   L040F
-         bra   L0439
-L0432    sta   >$FF40
+L0489    pshs  a
+         lda   >-u00A2,u
+         beq   L049F
+         ldb   #$0C
+         stb   >nh_data
+         lda   #$D8
+         sta   >$FF48
+         clr   u0004,u
+         bra   L04A3
+L049F    lda   <u008A
+         beq   L04A7
+L04A3    bsr   L0470
+         bra   L04AE
+L04A7    sta   >$FF40
          clr   u000E,u
          clr   <u0032
-L0439    puls  pc,a
-L043B    ldx   <$1E,y
+L04AE    puls  pc,a
+L04B0    ldx   <$1E,y
          ldb   #$14
-L0440    clr   b,x
+L04B5    clr   b,x
          decb  
-         bpl   L0440
+         bpl   L04B5
          ldb   <$26,y
          lda   <$27,y
          mul   
