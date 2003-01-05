1cb77719dc90ae4d180cb24ed5f528ecde18f629
boisy <unknown>
2003-01-05 22:49:28 +0000

    Added with new rammer

---
+++ level2/modules/md.asm
@@ -0,0 +1,36 @@
+********************************************************************
+* MD - RAMMER Memory Device Descriptor
+*
+* $Id$
+*
+* Ed.    Comments                                       Who YY/MM/DD
+* ------------------------------------------------------------------
+*        NitrOS-9 2.00 distribution                         ??/??/??
+
+         nam   MD
+         ttl   RAMMER Memory Device Descriptor
+
+         ifp1  
+         use   defsfile
+         endc  
+
+tylg     set   Devic+Objct
+atrv     set   ReEnt+rev
+rev      set   $01
+
+         mod   eom,name,tylg,atrv,mgrnam,drvnam
+
+         fcb   DIR.+SHARE.+PREAD.+PWRIT.+PEXEC.+READ.+WRITE.+EXEC. mode byte
+         fcb   HW.Page    extended controller address
+         fdb   $FFE0      physical controller address
+         fcb   initsize-*-1 initilization table size
+         fcb   DT.RBF     device type:0=scf,1=rbf,2=pipe,3=scf
+initsize equ   *
+
+name     fcs   /MD/
+mgrnam   fcs   /RBF/
+drvnam   fcs   /Rammer/
+
+         emod  
+eom      equ   *
+         end   
