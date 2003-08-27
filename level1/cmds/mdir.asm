d922d8e6ebd0bb7e448c97db09747c70f6ee11e5
boisy <unknown>
2003-08-27 20:53:51 +0000

    mdir fixes by Rodney!

--- level1/cmds/mdir.asm
+++ level1/cmds/mdir.asm
@@ -7,6 +7,7 @@
 * ------------------------------------------------------------------
 *   5    From Tandy OS-9 Level One VR 02.00.00
 *   6    Changed option to -e, optimized slightly       BGP 03/01/14
+*   7    Fixed leading zero supression, more optim      RVH 03/08/25
 
          nam   Mdir
          ttl   Show module directory
@@ -20,7 +21,7 @@
 tylg     set   Prgrm+Objct   
 atrv     set   ReEnt+rev
 rev      set   $01
-edition  set   6
+edition  set   7
 stdout   set   1
 
          mod   eom,name,tylg,atrv,start,size
@@ -29,23 +30,23 @@ stdout   set   1
 mdstart  rmb   2
 mdend    rmb   2
 parmptr  rmb   2
-u0006    rmb   1
+zflag    rmb   1	supress leading zeros flag
 bufptr   rmb   1
 u0008    rmb   1
 datebuf  rmb   3
 timebuf  rmb   3
-u000F    rmb   1
-u0010    rmb   1
+u000F    rmb   1	name field width
+u0010    rmb   1	last starting column
 narrow   rmb   1
-buffer   rmb   530
+buffer   rmb   80
+         rmb   450	stack & parameters
 size     equ   .
 
 name     fcs   /Mdir/
          fcb   edition
 
 tophead  fcb   C$LF
-         fcc   "  Module directory at "
-topheadl equ   *-tophead
+         fcs   "  Module directory at "
 ltitle   fcb   C$LF
          fcc   "Addr Size Typ Rev Attr Use Module name"
          fcb   C$LF
@@ -58,6 +59,7 @@ stitle   fcb   C$LF
          fcb   C$CR
 
 start    stx   <parmptr
+         clr   <zflag
          ldd   #$0C30
          std   <u000F
          clr   <narrow		assume wide output
@@ -73,44 +75,41 @@ L00D2    cmpx  #80		80 columns?
          inc   <narrow
          ldd   #$0A15
          std   <u000F
-L00DF    leax  >tophead,pcr
-         ldy   #topheadl
-         lda   #stdout
-         os9   I$WritLn 
-         leax  datebuf,u
-         os9   F$Time   
+L00DF    leay  >tophead,pcr
          leax  <buffer,u
          stx   <bufptr
+         lbsr  copySTR
+         leax  datebuf,u
+         os9   F$Time   
          leax  timebuf,u
          lbsr  L0224
-         lbsr  write
-         ldx   >D.ModDir
+         lbsr  writeBUF
+         ldx   >D.ModDir	MUST use ext addr for page 0
          stx   <mdstart
          ldd   >D.ModDir+2
          std   <mdend
-         leax  -$04,x
+         leax  -MD$ESize,x
 * Check for 'E' given as argument
          ldy   <parmptr
          ldd   ,y+
          andb  #$DF
          cmpd  #$2D45		-E ?	
-         bne   L0157
-         tst   <narrow
-         bne   L0123
+         bne   L015D
          leax  >ltitle,pcr
-         bra   L012B
-L0123    leax  >stitle,pcr
+         tst   <narrow
+         beq   L012B
+         leax  >stitle,pcr
 L012B    ldy   #80		max. length to write
          lda   #stdout
          os9   I$WritLn 
          ldx   <mdstart
-         lbra  L01B9
-loop     ldy   ,x
-         beq   L015D
-         ldd   $04,y
+         bra   L01B9
+loop     ldy   MD$MPtr,x
+         beq   L015D		skip if unused slot
+         ldd   M$Name,y
          leay  d,y
-         lbsr  L0205
-L0141    lbsr  L01F2
+         lbsr  copySTR
+L0141    lbsr  outSP
          ldb   <u0008
          subb  #$12
          cmpb  <u0010
@@ -119,91 +118,87 @@ L014C    subb  <u000F
          bhi   L014C
          bne   L0141
          bra   L015D
-L0154    lbsr  write
-L0157    leay  <buffer,u
-         sty   <bufptr
-L015D    leax  $04,x
+L0154    lbsr  writeBUF
+L015D    leax  MD$ESize,x
          cmpx  <mdend
          bcs   loop
-         lbsr  write
+         lbsr  writeBUF
          bra   ExitOk
 *
 * A module entry is 2 two byte pointers.
 * If the first pointer is $0000, then the slot is unused
-L0168    leay  <buffer,u
-         sty   <bufptr
-         ldy   ,x
-         beq   gotonxt         Is slot unused? If yes, branch
-         ldd   ,x
-         bsr   L01C1
-         ldd   $02,y
-         bsr   L01C1
+L0168    ldy   MD$MPtr,x	ptr=0?
+         beq   gotonxt		yes, skip unused slot
+         ldd   MD$MPtr,x	address (faster than tfr)
+         bsr   out4HS
+         ldd   M$Size,y		size
+         bsr   out4HS
          tst   <narrow
          bne   L0181
-         bsr   L01F2
-L0181    lda   $06,y
-         bsr   L01C9
+         bsr   outSP
+L0181    lda   M$Type,y		type/lang
+         bsr   out2HS
          tst   <narrow
          bne   L018B
-         bsr   L01F2
-L018B    lda   $07,y
-         anda  #$0F
-         bsr   L01C9
-         ldb   $07,y
-         lda   #$72
-         bsr   L01FE
+         bsr   outSP
+L018B    lda   M$Revs,y		revision
+         anda  #RevsMask
+         bsr   out2HS
+         ldb   M$Revs,y		attributes
+         lda   #'r
+         bsr   L01FE		bit 7 (ReEnt)
          tst   <narrow
          bne   L01A7
-         lda   #$3F
+         lda   #'w		bit 6 (ModProt:1=writable)
          bsr   L01FE
-         lda   #$3F
+         lda   #'3		bit 5 (ModNat:6309 Native mode)
          bsr   L01FE
-         lda   #$3F
+         lda   #'?		bit 4 undefined
          bsr   L01FE
-L01A7    bsr   L01F2
-         bsr   L01F2
-         lda   $02,x
-         bsr   L01C9
-         ldd   $04,y
-         leay  d,y
-         bsr   L0205
-         bsr   write
-gotonxt  leax  $04,x
+L01A7    bsr   outSP
+         bsr   outSP
+         lda   MD$Link,x	user count
+         bsr   out2HS
+         ldd   M$Name,y
+         leay  d,y		module name
+         bsr   copySTR
+         bsr   writeBUF
+gotonxt  leax  MD$ESize,x
 L01B9    cmpx  <mdend
          bcs   L0168
 
 ExitOk   clrb  
 Exit     os9   F$Exit   
 
-L01C1    bsr   Byt2Hex
+out4HS   inc   <zflag	supress leading zeros
+         inc   <zflag
+         bsr   Byt2Hex
+         dec   <zflag
          tfr   b,a
-         bsr   L01CF
-         bra   L01F2
-L01C9    bsr   Byt2Hex
-         bra   L01F2
+out2HS   bsr   Byt2Hex
+         bra   outSP
 
-Byt2Hex  clr   <u0006
-L01CF    pshs  a
+Byt2Hex  inc   <zflag	supress leading zero
+         pshs  a
          lsra  
          lsra  
          lsra  
          lsra  
          bsr   L01DB
-         lda   ,s+
-         anda  #$0F
-L01DB    tsta  
-         beq   L01E0
-         sta   <u0006
-L01E0    tst   <u0006
-         bne   L01E8
-         lda   #C$SPAC
-         bra   ApndA
-L01E8    adda  #'0
+         puls  a
+         anda  #$0F	is this a zero?
+L01DB    bne   L01E8	no, print it
+         tst   <zflag	still supressing zeros?
+         bgt   outZSP	yes, count it and print space
+L01E8    clr   <zflag	nonzero, print all the rest
+         adda  #'0
          cmpa  #'9
          bls   ApndA
-         adda  #$07    Make it A-F
+         adda  #$07	Make it A-F
          bra   ApndA
-L01F2    lda   #$20
+
+outZSP   dec   <zflag	countdown to last digit
+outSP    lda   #$20	append a space
 *
 * append a char (in reg a) to buffer
 *
@@ -212,43 +207,46 @@ ApndA    pshs  x
          sta   ,x+
          stx   <bufptr
          puls  pc,x
-
+*
+* process attribute flag bit
+*
 L01FE    rolb  
          bcs   ApndA
          lda   #'.
          bra   ApndA
-L0205    lda   ,y
+*
+* Copy an FCS string to buffer
+*
+copySTR  lda   ,y
          anda  #$7F
          bsr   ApndA
          lda   ,y+
-         bpl   L0205
+         bpl   copySTR
          rts   
 *
 * Append a CR to buffer and write it
 *
-write    pshs  y,x,a
+writeBUF pshs  y,x,a
          lda   #C$CR
          bsr   ApndA
          leax  <buffer,u
+         stx   <bufptr
          ldy   #80
          lda   #stdout
          os9   I$WritLn 
          puls  pc,y,x,a
 
+* Write the time to the buffer as HH:MM:SS
 L0224    bsr   Byt2ASC
          bsr   Colon
 Colon    lda   #':
          bsr   ApndA
 Byt2ASC  ldb   ,x+
-         lda   #$2F		load A with '0 - 1
-Hundreds inca  
-         subb  #100
+Hundreds subb  #100
          bcc   Hundreds
-         cmpa  #'0
-         beq   Tens		no leading zeros
-         bsr   ApndA
-Tens     lda   #$3A		load A with '9 + 1
-TensLoop deca  
+* code to print 100's digit removed - max timefield value is 59
+Tens     lda   #'9+1
+TensLoop deca
          addb  #10
          bcc   TensLoop
          bsr   ApndA
