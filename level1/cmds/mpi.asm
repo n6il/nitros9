35e21c0c0d865d5c8e311a4eec131a93fd44d9db
boisy <unknown>
2004-05-10 14:35:28 +0000

    Fixed a bug in the mpi utility

--- level1/cmds/mpi.asm
+++ level1/cmds/mpi.asm
@@ -8,6 +8,10 @@
 * ------------------------------------------------------------------
 *   1      2004/03/16  Rodney Hamilton
 * Created.
+*
+*   1r1    2004/05/08  Boisy G. Pitre
+* Fixed a problem where wrong message was being reported due to buggy
+* code, placed there by me :(
 
          nam   mpi
          ttl   Determines if a Multi-Pak is present
@@ -18,7 +22,7 @@
 
 tylg     set   Prgrm+Objct   
 atrv     set   ReEnt+rev
-rev      set   $00
+rev      set   $01
 edition  set   1
 
          mod   eom,name,tylg,atrv,start,size
@@ -35,27 +39,24 @@ mpi	fcc   "MPI found"
 	fcb   C$CR
 
 start	equ   *
-        tfr   cc,a
-        pshs  a
-	ldx   #$FF7F
-	lda   #$CC
-        orcc  #IntMasks
-	eora  ,x
-	sta   ,x
-	cmpa  ,x
-        puls  a
-        tfr   a,cc
-	beq   is_mpi
-	leax  <no,pc
-	bra   doit
-is_mpi	leax  <mpi,pc
-doit	equ   *
+        tfr   cc,b			save CC into B
+	ldx   #$FF7F			get MPI slot sel reg contents
+	lda   #$CC			load A with %11001100
+        orcc  #IntMasks			mask interrupts from this point on
+	eora  ,x			XOR with MPI slot sel reg contents
+	sta   ,x			and save...
+	cmpa  ,x			same?
+	beq   is_mpi			branch if so
+	leax  <no,pc			else point to "no"...
+	bra   doit			and DO IT!
+is_mpi	leax  <mpi,pc			or point to "yes"...
+doit	tfr   b,cc			restore original CC
 	ldy   #100
 	lda   #$01
-	os9   I$WritLn 
+	os9   I$WritLn 			write the message at ,X
 	bcs   L001C
 	clrb 
-L001C	os9   F$Exit 
+L001C	os9   F$Exit 			exit...
 
 	emod
 eom	equ   *
