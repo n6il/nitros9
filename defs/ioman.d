ce02e2bec40856450015d990a0c597fcdd921d2a
Boisy Pitre <coco@toughmac.com>
2005-03-23 03:21:41 +0000

    Updated for new as6809

--- defs/ioman.d
+++ defs/ioman.d
@@ -1,21 +1,21 @@
-**************************************
-* I/O Service Request Code Definitions
-*
-         org   $80
-I$Attach rmb   1          Attach I/O Device
-I$Detach rmb   1          Detach I/O Device
-I$Dup    rmb   1          Duplicate Path
-I$Create rmb   1          Create New File
-I$Open   rmb   1          Open Existing File
-I$MakDir rmb   1          Make Directory File
-I$ChgDir rmb   1          Change Default Directory
-I$Delete rmb   1          Delete File
-I$Seek   rmb   1          Change Current Position
-I$Read   rmb   1          Read Data
-I$Write  rmb   1          Write Data
-I$ReadLn rmb   1          Read Line of ASCII Data
-I$WritLn rmb   1          Write Line of ASCII Data
-I$GetStt rmb   1          Get Path Status
-I$SetStt rmb   1          Set Path Status
-I$Close  rmb   1          Close Path
-I$DeletX rmb   1          Delete from current exec dir
+;*************************************
+; I/O Service Request Code Definitions
+;
+
+I$Attach ==   0h80          ; Attach I/O Device
+I$Detach ==   0h80          ; Detach I/O Device
+I$Dup    ==   0h80          ; Duplicate Path
+I$Create ==   0h80          ; Create New File
+I$Open   ==   0h80          ; Open Existing File
+I$MakDir ==   0h80          ; Make Directory File
+I$ChgDir ==   0h80          ; Change Default Directory
+I$Delete ==   0h80          ; Delete File
+I$Seek   ==   0h80          ; Change Current Position
+I$Read   ==   0h80          ; Read Data
+I$Write  ==   0h80          ; Write Data
+I$ReadLn ==   0h80          ; Read Line of ASCII Data
+I$WritLn ==   0h80          ; Write Line of ASCII Data
+I$GetStt ==   0h80          ; Get Path Status
+I$SetStt ==   0h80          ; Set Path Status
+I$Close  ==   0h80          ; Close Path
+I$DeletX ==   0h80          ; Delete from current exec dir
--- defs/syscall.d
+++ defs/syscall.d
@@ -25,145 +25,122 @@ false      ==     0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; System Service Request Code Definitions
 ;
-           .org   0
-F$Link::   .rmb   1          ; Link to Module
-F$Load::   .rmb   1          ; Load Module from File
-F$UnLink:: .rmb   1          ; Unlink Module
-F$Fork::   .rmb   1          ; Start New Process
-F$Wait::   .rmb   1          ; Wait for Child Process to Die
-F$Chain::  .rmb   1          ; Chain Process to New Module
-F$Exit::   .rmb   1          ; Terminate Process
-F$Mem::    .rmb   1          ; Set Memory Size
-F$Send::   .rmb   1          ; Send Signal to Process
-F$Icpt::   .rmb   1          ; Set Signal Intercept
-F$Sleep::  .rmb   1          ; Suspend Process
-F$SSpd::   .rmb   1          ; Suspend Process
-F$ID::     .rmb   1          ; Return Process ID
-F$SPrior:: .rmb   1          ; Set Process Priority
-F$SSWI::   .rmb   1          ; Set Software Interrupt
-F$PErr::   .rmb   1          ; Print Error
-F$PrsNam:: .rmb   1          ; Parse Pathlist Name
-F$CmpNam:: .rmb   1          ; Compare Two Names
-F$SchBit:: .rmb   1          ; Search Bit Map
-F$AllBit:: .rmb   1          ; Allocate in Bit Map
-F$DelBit:: .rmb   1          ; Deallocate in Bit Map
-F$Time::   .rmb   1          ; Get Current Time
-F$STime::  .rmb   1          ; Set Current Time
-F$CRC::    .rmb   1          ; Generate CRC
+F$Link     ==   0h00          ; Link to Module
+F$Load     ==   0h01          ; Load Module from File
+F$UnLink   ==   0h02          ; Unlink Module
+F$Fork     ==   0h03          ; Start New Process
+F$Wait     ==   0h04          ; Wait for Child Process to Die
+F$Chain    ==   0h05          ; Chain Process to New Module
+F$Exit     ==   0h06          ; Terminate Process
+F$Mem      ==   0h07          ; Set Memory Size
+F$Send     ==   0h08          ; Send Signal to Process
+F$Icpt     ==   0h09          ; Set Signal Intercept
+F$Sleep    ==   0h0A          ; Suspend Process
+F$SSpd     ==   0h0B          ; Suspend Process
+F$ID       ==   0h0C          ; Return Process ID
+F$SPrior   ==   0h0D          ; Set Process Priority
+F$SSWI     ==   0h0E          ; Set Software Interrupt
+F$PErr     ==   0h0F          ; Print Error
+F$PrsNam   ==   0h10          ; Parse Pathlist Name
+F$CmpNam   ==   0h11          ; Compare Two Names
+F$SchBit   ==   0h12          ; Search Bit Map
+F$AllBit   ==   0h13          ; Allocate in Bit Map
+F$DelBit   ==   0h14          ; Deallocate in Bit Map
+F$Time     ==   0h15          ; Get Current Time
+F$STime    ==   0h16          ; Set Current Time
+F$CRC      ==   0h17          ; Generate CRC
 
            .ifgt  Level-1
 
 ; NitrOS-9 Level 2 system calls
-F$GPrDsc:: .rmb   1          ; Get Process Descriptor copy
-F$GBlkMp:: .rmb   1          ; Get System Block Map copy
-F$GModDr:: .rmb   1          ; Get Module Directory copy
-F$CpyMem:: .rmb   1          ; Copy External Memory
-F$SUser::  .rmb   1          ; Set User ID number
-F$UnLoad:: .rmb   1          ; Unlink Module by name
-F$Alarm::  .rmb   1          ; Color Computer 3 Alarm Call
-           .rmb   2          ; Reserved - For overlap of other systems
-F$NMLink:: .rmb   1          ; Color Computer 3 Non-Mapping Link
-F$NMLoad:: .rmb   1          ; Color Computer 3 Non-Mapping Load
-           .org   0h25
-F$TPS::    .rmb   1          ; Return System's Ticks Per Second
-F$TimAlm:: .rmb   1          ; CoCo individual process alarm call
+F$GPrDsc   ==   0h18          ; Get Process Descriptor copy
+F$GBlkMp   ==   0h19          ; Get System Block Map copy
+F$GModDr   ==   0h1A          ; Get Module Directory copy
+F$CpyMem   ==   0h1B          ; Copy External Memory
+F$SUser    ==   0h1C          ; Set User ID number
+F$UnLoad   ==   0h1D          ; Unlink Module by name
+F$Alarm    ==   0h1E          ; Color Computer 3 Alarm Call
+
+F$NMLink   ==   0h21          ; Color Computer 3 Non-Mapping Link
+F$NMLoad   ==   0h22          ; Color Computer 3 Non-Mapping Load
+
+F$TPS      ==   0h25          ; Return System's Ticks Per Second
+F$TimAlm   ==   0h26          ; CoCo individual process alarm call
 
            .endif
 
-           .org   0h27       ; Beginning of System Reserved Calls
-F$VIRQ::   .rmb   1          ; Install/Delete Virtual IRQ
-F$SRqMem:: .rmb   1          ; System Memory Request
-F$SRtMem:: .rmb   1          ; System Memory Return
-F$IRQ::    .rmb   1          ; Enter IRQ Polling Table
-F$IOQu::   .rmb   1          ; Enter I/O Queue
-F$AProc::  .rmb   1          ; Enter Active Process Queue
-F$NProc::  .rmb   1          ; Start Next Process
-F$VModul:: .rmb   1          ; Validate Module
-F$Find64:: .rmb   1          ; Find Process/Path Descriptor
-F$All64::  .rmb   1          ; Allocate Process/Path Descriptor
-F$Ret64::  .rmb   1          ; Return Process/Path Descriptor
-F$SSvc::   .rmb   1          ; Service Request Table Initialization
-F$IODel::  .rmb   1          ; Delete I/O Module
+; Beginning of System Reserved Calls
+F$VIRQ     ==   0h27          ; Install/Delete Virtual IRQ
+F$SRqMem   ==   0h28          ; System Memory Request
+F$SRtMem   ==   0h29          ; System Memory Return
+F$IRQ      ==   0h2A          ; Enter IRQ Polling Table
+F$IOQu     ==   0h2B          ; Enter I/O Queue
+F$AProc    ==   0h2C          ; Enter Active Process Queue
+F$NProc    ==   0h2D          ; Start Next Process
+F$VModul   ==   0h2E          ; Validate Module
+F$Find64   ==   0h2F          ; Find Process/Path Descriptor
+F$All64    ==   0h30          ; Allocate Process/Path Descriptor
+F$Ret64    ==   0h31          ; Return Process/Path Descriptor
+F$SSvc     ==   0h32          ; Service Request Table Initialization
+F$IODel    ==   0h33          ; Delete I/O Module
 
            .ifgt  Level-1
 
-F$SLink::  .rmb   1          ; System Link
-F$Boot::   .rmb   1          ; Bootstrap System
-F$BtMem::  .rmb   1          ; Bootstrap Memory Request
-F$GProcP:: .rmb   1          ; Get Process ptr
-F$Move::   .rmb   1          ; Move Data (low bound first)
-F$AllRAM:: .rmb   1          ; Allocate RAM blocks
-F$AllImg:: .rmb   1          ; Allocate Image RAM blocks
-F$DelImg:: .rmb   1          ; Deallocate Image RAM blocks
-F$SetImg:: .rmb   1          ; Set Process DAT Image
-F$FreeLB:: .rmb   1          ; Get Free Low Block
-F$FreeHB:: .rmb   1          ; Get Free High Block
-F$AllTsk:: .rmb   1          ; Allocate Process Task number
-F$DelTsk:: .rmb   1          ; Deallocate Process Task number
-F$SetTsk:: .rmb   1          ; Set Process Task DAT registers
-F$ResTsk:: .rmb   1          ; Reserve Task number
-F$RelTsk:: .rmb   1          ; Release Task number
-F$DATLog:: .rmb   1          ; Convert DAT Block/Offset to Logical
-F$DATTmp:: .rmb   1          ; Make temporary DAT image (Obsolete)
-F$LDAXY::  .rmb   1          ; Load A [X,[Y]]
-F$LDAXYP:: .rmb   1          ; Load A [X+,[Y]]
-F$LDDDXY:: .rmb   1          ; Load D [D+X,[Y]]
-F$LDABX::  .rmb   1          ; Load A from 0,X in task B
-F$STABX::  .rmb   1          ; Store A at 0,X in task B
-F$AllPrc:: .rmb   1          ; Allocate Process Descriptor
-F$DelPrc:: .rmb   1          ; Deallocate Process Descriptor
-F$ELink::  .rmb   1          ; Link using Module Directory Entry
-F$FModul:: .rmb   1          ; Find Module Directory Entry
-F$MapBlk:: .rmb   1          ; Map Specific Block
-F$ClrBlk:: .rmb   1          ; Clear Specific Block
-F$DelRAM:: .rmb   1          ; Deallocate RAM blocks
-F$GCMDir:: .rmb   1          ; Pack module directory
-F$AlHRAM:: .rmb   1          ; Allocate HIGH RAM Blocks
+F$SLink    ==   0h34          ; System Link
+F$Boot     ==   0h35          ; Bootstrap System
+F$BtMem    ==   0h36          ; Bootstrap Memory Request
+F$GProcP   ==   0h37          ; Get Process ptr
+F$Move     ==   0h38          ; Move Data (low bound first)
+F$AllRAM   ==   0h39          ; Allocate RAM blocks
+F$AllImg   ==   0h3A          ; Allocate Image RAM blocks
+F$DelImg   ==   0h3B          ; Deallocate Image RAM blocks
+F$SetImg   ==   0h3C          ; Set Process DAT Image
+F$FreeLB   ==   0h3D          ; Get Free Low Block
+F$FreeHB   ==   0h3E          ; Get Free High Block
+F$AllTsk   ==   0h3F          ; Allocate Process Task number
+F$DelTsk   ==   0h40          ; Deallocate Process Task number
+F$SetTsk   ==   0h41          ; Set Process Task DAT registers
+F$ResTsk   ==   0h42          ; Reserve Task number
+F$RelTsk   ==   0h43          ; Release Task number
+F$DATLog   ==   0h44          ; Convert DAT Block/Offset to Logical
+F$DATTmp   ==   0h45          ; Make temporary DAT image (Obsolete)
+F$LDAXY    ==   0h46          ; Load A [X,[Y]]
+F$LDAXYP   ==   0h47          ; Load A [X+,[Y]]
+F$LDDDXY   ==   0h48          ; Load D [D+X,[Y]]
+F$LDABX    ==   0h49          ; Load A from 0,X in task B
+F$STABX    ==   0h4A          ; Store A at 0,X in task B
+F$AllPrc   ==   0h4B          ; Allocate Process Descriptor
+F$DelPrc   ==   0h4C          ; Deallocate Process Descriptor
+F$ELink    ==   0h4D          ; Link using Module Directory Entry
+F$FModul   ==   0h4E          ; Find Module Directory Entry
+F$MapBlk   ==   0h4F          ; Map Specific Block
+F$ClrBlk   ==   0h50          ; Clear Specific Block
+F$DelRAM   ==   0h51          ; Deallocate RAM blocks
+F$GCMDir   ==   0hh52          ; Pack module directory
+F$AlHRAM   ==   0h53          ; Allocate HIGH RAM Blocks
 
 ; Alan DeKok additions
 ; F$ReBoot is unimplemented at this time
-F$ReBoot:: .rmb   1          ; Reboot machine (reload OS9Boot) or drop to RSDOS
-F$CRCMod:: .rmb   1          ; CRC mode, toggle or report current status
-F$XTime::  .rmb   1          ; Get Extended time packet from RTC (fractions of second)
-F$VBlock:: .rmb   1          ; Verify modules in a block of memory, add to module directory
+F$ReBoot   ==   0h54          ; Reboot machine (reload OS9Boot) or drop to RSDOS
+F$CRCMod   ==   0h55          ; CRC mode, toggle or report current status
+F$XTime    ==   0h56          ; Get Extended time packet from RTC (fractions of second)
+F$VBlock   ==   0h57          ; Verify modules in a block of memory, add to module directory
 
            .endif
 
 ;
 ; Numbers $70 through $7F are reserved for user definitions
 ;
-           .org   0h70
+;           .org   0h70
 
 	   .iflt  Level-2
 
-	   .rmb   16         ; Reserved for user definition
+;	   .rmb   16         ; Reserved for user definition
 
            .else
 
-F$RegDmp:: .rmb   1          ; Ron Lammardo's debugging register dump
-F$NVRAM::  .rmb   1          ; Non Volatile RAM (RTC battery backed static) read/write
+F$RegDmp   ==   0h70		; Ron Lammardo's debugging register dump
+F$NVRAM    ==   0h71          ; Non Volatile RAM (RTC battery backed static) read/write
                              ; Reserved for user definitions
 
            .endif
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; I/O Service Request Code Definitions
-;
-           .org   0h80
-I$Attach:: .rmb   1          ; Attach I/O Device
-I$Detach:: .rmb   1          ; Detach I/O Device
-I$Dup::    .rmb   1          ; Duplicate Path
-I$Create:: .rmb   1          ; Create New File
-I$Open::   .rmb   1          ; Open Existing File
-I$MakDir:: .rmb   1          ; Make Directory File
-I$ChgDir:: .rmb   1          ; Change Default Directory
-I$Delete:: .rmb   1          ; Delete File
-I$Seek::   .rmb   1          ; Change Current Position
-I$Read::   .rmb   1          ; Read Data
-I$Write::  .rmb   1          ; Write Data
-I$ReadLn:: .rmb   1          ; Read Line of ASCII Data
-I$WritLn:: .rmb   1          ; Write Line of ASCII Data
-I$GetStt:: .rmb   1          ; Get Path Status
-I$SetStt:: .rmb   1          ; Set Path Status
-I$Close::  .rmb   1          ; Close Path
-I$DeletX:: .rmb   1          ; Delete from current exec dir
