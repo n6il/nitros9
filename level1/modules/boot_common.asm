ca1608713bd08ce41254562ac9eece405aa49d2e
Boisy Pitre <coco@toughmac.com>
2005-10-15 15:32:45 +0000

    Modified booters to use new boot strategy with boot_common.asm

---
+++ level1/modules/boot_common.asm
@@ -0,0 +1,162 @@
+********************************************************************
+* Boot Common - Common code for NitrOS-9 booters
+*
+* $Id$
+*
+* This common file is not a stand-alone module, but is 'used' by boot module
+* source files to bring in support for booting from RBF file systems that have
+* either standard or new-style fragmented bootfiles.
+*
+* This code uses several static variables which are expected to be defined in
+* the main boot source code.  See a booter like boot_1773.asm for an example on
+* how to write a booter which uses this code.
+*
+* FLOPPY BOOTERS: Take note.  Only the lower 16 bits of DD.BT are used, so
+* in that case, we can save some code by ignoring the loading of B as well as
+* loading bits 23-16 of DD.BT.  Floppy booters should have the following
+* line in their code to take advantage of this optimization:
+*
+* FLOPPY equ 1
+*
+* Edt/Rev  YYYY/MM/DD  Modified by
+* Comment
+* ------------------------------------------------------------------
+*   1      2005/10/14  Boisy G. Pitre
+* Created as a stand-alone file.
+                         
+start    orcc  #IntMasks  ensure IRQs are off (necessary?)
+         leas  -size,s   
+         tfr   s,u        get pointer to data area
+         pshs  u          save pointer to data area
+                         
+* Request memory for LSN0
+         ldd   #256       get sector/fd buffer
+         os9   F$SRqMem   get it!
+         bcs   error2    
+         bsr   getpntr    restore U to point to our statics
+                         
+* Initialize Hardware
+         lbsr  HWInit
+
+* Read LSN0
+         IFEQ             FLOPPY
+         clrb             MSB sector
+         ENDC
+         ldx   #0         LSW sector
+         lbsr  HWRead     read LSN 0
+         bcs   error      branch if error
+                         
+         ifgt  Level-1   
+         lda   #'0        --- loaded in LSN0'
+         jsr   <D.BtBug   ---
+         endc            
+                         
+* Pull relevant values from LSN0
+         IFNE  FLOPPY
+         lda   DD.TKS,x   number of tracks on this disk
+         sta   ddtks,u   
+         lda   DD.FMT,x   disk format byte
+         sta   ddfmt,u   
+         ELSE
+         lda   DD.BT,x    os9boot pointer
+         sta   bootloc,u 
+         ENDC
+         ldd   DD.BT+1,x  LSW of 24 bit address
+         std   bootloc+1,u
+         ldd   DD.BSZ,x   os9boot size in bytes
+         beq   FragBoot   if zero, do frag boot
+         std   bootsize,u
+* Old style boot -- make a fake FD segment
+         leax  FD.SEG,x  
+         addd  #$00FF     round up to next page
+* Important note: We are making an assumption that the upper 8 bits of the
+* FDSL.B field will always be zero.  That is a safe assumption, since an
+* FDSL.B value of $00FF would mean the file is 65280 bytes.  A bootfile
+* under NitrOS-9 cannot be this large, and therefore this assumption
+* is safe.
+         sta   FDSL.B+1,x save file size
+         IFEQ  FLOPPY
+         lda   bootloc,u 
+         sta   FDSL.A,x  
+         ENDC
+         ldd   bootloc+1,u
+         std   FDSL.A+1,x save LSN of file (contiguous)
+         clr   FDSL.S,x   make next segment entry 0
+         clr   FDSL.S+1,x
+         clr   FDSL.S+2,x
+         ldd   bootsize,u
+         bra   GrabBootMem
+                         
+Back2Krn lbsr  HWTerm     call HW termination routine
+         ldx   blockimg,u pointer to start of os9boot in memory
+         clrb             clear carry
+         ldd   bootsize,u
+error2   leas  2+size,s   reset the stack    same as PULS U
+         rts              return to kernel
+                         
+* Error point - return allocated memory and then return to kernel
+error                    
+* Return memory allocated for sector buffers
+         ldd   #256      
+         ldu   blockloc,u
+         os9   F$SRtMem  
+         bra   error2    
+                         
+* Routine to save off alloced mem from F$SRqMem into blockloc,u and restore
+* the statics pointer in U
+getpntr  tfr   u,d        save pointer to requested memory
+         ldu   2,s        recover pointer to data stack
+         std   blockloc,u
+         rts             
+                         
+* NEW! Fragmented boot support!
+FragBoot ldb   bootloc,u  MSB fd sector location
+         ldx   bootloc+1,u LSW fd sector location
+         lbsr  HWRead     get fd sector
+         ldd   FD.SIZ+2,x get file size (we skip first two bytes)
+         std   bootsize,u
+         leax  FD.SEG,x   point to segment table
+                         
+GrabBootMem                 
+         ifgt  Level-1   
+         os9   F$BtMem   
+         else            
+         os9   F$SRqMem  
+         endc            
+         bcs   error     
+         bsr   getpntr   
+         std   blockimg,u
+                         
+* Get os9boot into memory
+BootLoop stx   seglist,u  update segment list
+         IFEQ  FLOPPY
+         ldb   FDSL.A,x   MSB sector location
+         ENDC
+BL2      ldx   FDSL.A+1,x LSW sector location
+         bne   BL3       
+         tstb            
+         beq   Back2Krn  
+BL3      lbsr  HWRead    
+         inc   blockloc,u point to next input sector in mem
+                         
+         ifgt  Level-1   
+         lda   #'.        Show .'
+         jsr   <D.BtBug  
+         endc            
+                         
+         ldx   seglist,u  get pointer to segment list
+         dec   FDSL.B+1,x get segment size
+         beq   NextSeg    if <=0, get next segment
+                         
+         ldd   FDSL.A+1,x update sector location by one to 24bit word
+         addd  #1        
+         std   FDSL.A+1,x
+         IFEQ  FLOPPY
+         ldb   FDSL.A,x  
+         adcb  #0        
+         stb   FDSL.A,x  
+         ENDC
+         bra   BL2       
+                         
+NextSeg  leax  FDSL.S,x   advance to next segment entry
+         bra   BootLoop  
