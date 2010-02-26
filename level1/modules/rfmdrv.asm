d7c902a29908f39c5d04c3f6f4bfb909f06ad771
boisy <unknown>
2010-02-26 22:15:18 +0000

    Fixed issue with rfmdrv.asm... should have all entry points

--- level1/modules/rfmdrv.asm
+++ level1/modules/rfmdrv.asm
@@ -17,18 +17,29 @@ edition  set   $01
          mod   eom,name,tylg,atrv,start,size
 
         
-wtf  rmb   1
 size     equ   .
 
- 		 fcb   DIR.+SHARE.+PEXEC.+PWRIT.+PREAD.+EXEC.+UPDAT.
+         fcb   DIR.+SHARE.+PEXEC.+PWRIT.+PREAD.+EXEC.+UPDAT.
 
 
 name     fcs   /rfmdrv/
          fcb   edition
 
-start    nop
-		 clrb
-		 rts
+start    lbra   Init
+         lbra   Read
+         lbra   Write
+         lbra   GetStat
+         lbra   SetStat
+*         lbra   Term
+         
+Init
+Read
+Write
+GetStat
+SetStat
+         nop
+	 clrb
+	 rts
 		 
          emod
 eom      equ   *
