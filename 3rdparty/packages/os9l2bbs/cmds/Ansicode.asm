a3957834c4050d67f2299ab65dba8875feb0fdce
Lothan <lothan@newsguy.com>
2010-04-01 01:22:00 +0000

    Commented Ansicode.asm and changed the code at 0015 and 0016 to data.

--- 3rdparty/packages/os9l2bbs/cmds/Ansicode.asm
+++ 3rdparty/packages/os9l2bbs/cmds/Ansicode.asm
@@ -1,32 +1,33 @@
-         nam   Ansicode
-         ttl   program module       
+           nam    Ansicode
+           ttl    program module
 
-* Disassembled 2010/01/24 10:52:29 by Disasm v1.5 (C) 1988 by RML
+           ifp1
+           use    defsfile
+           endc
 
-         ifp1
-         use   defsfile
-         endc
-tylg     set   Prgrm+Objct   
-atrv     set   ReEnt+rev
-rev      set   $01
-         mod   eom,name,tylg,atrv,start,size
-u0000    rmb   400
-size     equ   .
-name     equ   *
-         fcs   /Ansicode/
-L0015    fcb   $1B 
-         fcb   $5B [
-start    equ   *
-         pshs  x
-         leax  >L0015,pcr
-         ldy   #$0002
-         lda   #$01
-         os9   I$Write  
-         puls  x
-         ldy   #$00C8
-         os9   I$WritLn 
-         clrb  
-         os9   F$Exit   
-         emod
-eom      equ   *
-         end
+tylg       set    Prgrm+Objct
+atrv       set    ReEnt+rev
+rev        set    $01
+
+           mod    eom,name,tylg,atrv,start,size
+
+U0000      rmb    400
+size       equ    .
+
+name       fcs    /Ansicode/                                            # 000D 41 6E 73 69 63 6F 64 E5 Ansicode
+L0015      fcb    $1B        # ANSI escape sequence                     # 0015 1B             .
+           fcb    $5B                                                   # 0016 5B             [
+start      pshs   X          # Save the pointer to the parameter string # 0017 34 10          4.
+           leax   L0015,PC   # Point to the ANSIC escape sequence       # 0019 30 8D FF F8    0..x
+           ldy    #2         # Write two characters                     # 001D 10 8E 00 02    ....
+           lda    #$01       # to standard output                       # 0021 86 01          ..
+           os9    I$Write    # Write the ANSI escape sequence           # 0023 10 3F 8A       .?.
+           puls   X          # Restore the pointer to the parameter string # 0026 35 10          5.
+           ldy    #200       # Write a maximum of 200 characters        # 0028 10 8E 00 C8    ...H
+           os9    I$WritLn   # Write the parameter string               # 002C 10 3F 8C       .?.
+           clrb              # Clear the error code ...                 # 002F 5F             _
+           os9    F$Exit     # ... and exit                             # 0030 10 3F 06       .?.
+
+           emod
+eom        equ    *
+           end
