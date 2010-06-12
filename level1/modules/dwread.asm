31b71eb94e9dfdc7daf728ef8a8bba2832128f8c
Boisy Pitre <coco@toughmac.com>
2010-06-12 11:59:39 +0000

    Added code for Gary's CoCo3FPGA interface

--- level1/modules/dwread.asm
+++ level1/modules/dwread.asm
@@ -18,6 +18,51 @@
 *
 
 
+          IFNE BECKER
+DWRead    clra                          ; clear Carry (no framing error)
+          deca                          ; clear Z flag, A = timeout msb ($ff)
+          tfr       cc,b
+          pshs      u,x,b,a          ; preserve registers, push timeout msb
+          orcc      #$50                ; mask interrupts
+;          tfr       a,dp                ; set direct page to $FFxx
+;          setdp     $ff
+          leau      ,x                  ; U = storage ptr
+          ldx       #0                  ; initialize checksum
+          adda      #2                  ; A = $01 (serial in mask), set Carry
+
+* Wait for byte or timeout
+rx0010    bcc       rxExit              ; exit if timeout expired
+          ldb       #$ff                ; init timeout lsb
+rx0020    lda       $FF41               ; check for byte ready
+          bita      #$01
+          bne       rxByte              ; branch if start bit detected
+          subb      #1                  ; decrement timeout lsb
+          bcc       rx0020              ; loop until timeout lsb rolls under
+          beq       rxByte
+          addb      ,s                  ; B = timeout msb - 1
+          stb       ,s                  ; store decremented timeout msb
+          bra       rx0010              ; loop if still no start bit
+
+* Read a byte
+rxByte    ldb       $FF42
+
+          stb       ,u+                 ; store received byte to memory
+          abx                           ; update checksum
+          ldb       #$ff                ; set timeout lsb for next byte
+          leay      ,y                  ; test request count
+          bne       rx0020              ; loop if another byte wanted
+          lda       #$03                ; setup to return SUCCESS
+
+* Clean up, set status and return
+rxExit    leas      1,s                 ; remove timeout msb from stack
+          inca                          ; A = status to be returned in C and Z
+          ora       ,s                  ; place status information into the..
+          sta       ,s                  ; ..C and Z bits of the preserved CC
+          leay      ,x                  ; return checksum in Y
+          puls      cc,x,u,pc        ; restore registers and return
+;          setdp     $00
+
+          ELSE
           IFNE BAUD38400
 *******************************************************
 * 38400 bps using 6809 code and timimg
@@ -240,5 +285,4 @@ rx0050    inca                          ; A = status to be returned in C and Z
 
           ENDC
           ENDC
-
-
+          ENDC
