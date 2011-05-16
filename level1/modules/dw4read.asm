58632ba0bc344e0f04d2ef32c08f1e4d90a61be7
boisy <unknown>
2011-05-16 00:57:24 +0000

    Added 6309 code from Darren

--- level1/modules/dw4read.asm
+++ level1/modules/dw4read.asm
@@ -1,3 +1,137 @@
+	IFNE	H6309-1
+
+*******************************************************
+*
+* DWRead  -  6309 native Turbo Edition  115k / 230k
+*    Receive a response from the DriveWire server.
+*    Times out if no data received within 1.3 (0.66) seconds.
+*
+*    THIS VERSION REQUIRES ONE OR MORE SYNC BYTES 
+*    WHERE THE THE FINAL SYNC BYTE IS $C0 AND ANY
+*    PRECEDING SYNC BYTES ARE $FF.
+*
+*    THE DATA BYTES MUST BE TRANSMITTED IN REVERSE
+*    BIT ORDER (MOST-SIGNIFICANT FIRST).
+*
+*    Serial data format:  8-N-2    (TWO STOP BITS MANDATORY)
+*
+* Entry:
+*    X  = storage address for incoming data
+*    Y  = number of bytes requested
+*
+* Exit:
+*    CC = Z set on success, cleared on timeout
+*    Y  = checksum
+*    U is preserved
+*    All others clobbered
+*
+BBIN	equ	$FF22		; bit banger input port
+
+DWRead	clra			; ACCA = 0, clear Carry
+	pshs	u,dp,a,cc		; save registers (A allocates space for status)
+	orcc	#$50		; mask interrupts
+	deca			; select hardware page..
+	tfr	a,dp		; ..as the direct page
+	ldu	#BBIN		; point U at input port
+	tfr	y,w		; W = request count
+	leay	,x		; Y = storage ptr
+	ldx	#0		; initialize timeout counter
+	stx	<$FF92		; disable GIME interrupts
+	ldd	<$FF92		; clear spurious interrupts
+
+* Turn off PIA interrupt sources except for the CD input pin.
+	lda	<$FF01		; save PIA 0 controls on the stack
+	ldb	<$FF03
+	pshs	d
+	andd	#$FCFC		; clear IRQ enables
+	sta	<$FF01		; set new control state for PIA 0
+	stb	<$FF03
+	lda	<$FF00		; ensure that IRQ outputs are cleared
+	ldb	<$FF02
+	lda	<$FF21		; save PIA 1 controls on the stack
+	ldb	<$FF23
+	pshs	d
+	andd	#$FCFC		; clear FIRQ enables
+	inca			; set CD FIRQ enable
+	sta	<$FF21		; set new control state for PIA 1
+	stb	<$FF23
+	
+* Wait for Sync Byte(s) or Timeout
+sync1	ldd	#$0102		; ACCA = serial in mask, ACCB = shift counter
+sync2	bita	,u		; sample input
+	beq	sync3		; branch if low
+	leax	-1,x		; decrement timeout counter
+	bne	sync1		; loop if timeout has not expired
+	bra	rxDone		; exit due to timeout
+sync3	lsrb			; have there been 2 consecutive low samples?
+	bcc	sync2		; keep waiting if no
+	ldx	#0		; initialize checksum
+sync4	bita	,u		; sample input
+	bne	rxByte		; branch if input has returned hi
+	rorb			; bump secondary timeout counter
+	bcc	sync4		; branch if not timeout
+	bra	rxDone		; exit due to timeout
+
+* Byte receiver loop
+rxByte	lda	<$FF20		; reset FIRQ
+	sync			; wait for start bit
+	leau	,u
+	lda	,u		; bit 0
+	lsra
+	rolb
+	lda	,u++		; bit 1
+	lsra
+	rolb
+	lda	-2,u		; bit 2
+	lsra
+	rolb
+	lda	,--u		; bit 3
+	lsra
+	rolb
+	lda	,u++		; bit 4
+	lsra
+	rolb
+	lda	,--u		; bit 5
+	lsra
+	rolb
+	lda	,u+		; bit 6
+	lsra
+	rolb
+	nop
+	lda	,-u		; bit 7
+	lsra
+	rolb
+	abx			; update checksum
+	stb	,y+		; put byte to storage
+	decw			; decrement counter
+	bne	rxByte		; loop if more bytes to read
+	clra			; status = SUCCESS
+rxDone	sta	5,s		; store status on stack
+
+* Restore previous PIA control settings
+	puls	d		; restore original PIA 1 controls
+	sta	<$FF21
+	stb	<$FF23
+	puls	d		; restore original PIA 0 controls
+	sta	<$FF01
+	stb	<$FF03
+	lda	<$FF20		; make sure the CD FIRQ has been cleared
+
+        IFNE    NITROS9-1
+* Restoration of GIME interrupts in NitrOS9
+	ldd	>D.IRQER		; retrieve shadow copy of IRQ/FIRQ enable regs
+	std	<$FF92		; restore GIME
+	ldd	<$FF92		; clear spurious interrupts
+        ENDIF
+
+* Clean up and return
+	leay	,x		; return checksum in Y
+	puls	cc,a,dp,u		; ACCA = status;  restore IRQ masks, DP and U
+	tsta			; set CC.Z if status = SUCCESS
+	rts			; return
+
+        ELSE
+
 *******************************************************
 *
 * DWRead  -  6809 Turbo Edition  115k / 230k
@@ -119,12 +253,12 @@ rxDone	sta	6,s		; store status on stack
 	stb	<$FF03
 	lda	<$FF20		; make sure the CD FIRQ has been cleared
 
-   IF NITROS
+        IFNE    NITROS9-1
 * Restoration of GIME interrupts in NitrOS9
 	ldd	>D.IRQER		; retrieve shadow copy of IRQ/FIRQ enable regs
 	std	<$FF92		; restore GIME
 	ldd	<$FF92		; clear spurious interrupts
-   ENDIF
+        ENDIF
 
 * Checksum computation
 	clrb			; initialize checksum LSB
@@ -138,3 +272,5 @@ sumLoop	addb	,x+		; get one byte and add to..
 	andcc	#$FE		; clear carry (no framing error)
 rxExit	tfr	d,y		; move checksum into Y without affecting CC.Z
 	rts			; return
+
+        ENDC
