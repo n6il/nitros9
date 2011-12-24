1b107bef08534e13b0b55e4de87078bac60f93ca
robertgault <unknown>
2011-12-24 13:05:41 +0000

    New descriptor for second vhd drive in MESS v144 and newer. R. Gault

---
+++ 3rdparty/drivers/emudsk/h1.asm
@@ -0,0 +1,45 @@
+********************************************************************
+* Emudsk - Virtual disk driver for CoCo emulators
+*
+* $Id$
+*
+* Ed.    Comments                                       Who YY/MM/DD
+* ------------------------------------------------------------------
+*  01    Modified to compile under OS9Source            tjl 02/08/28
+*  02    Corrected device address for MESS          R.Gault 11/12/24
+*        Address not used by driver but still ....
+
+ IFP1
+ USE os9defs
+ ENDC
+
+type SET Devic+Objct
+ MOD rend,rnam,type,ReEnt+1,fmnam,drvnam
+ FCB $FF  all access modes
+ FCB $07,$FF,$80 device address
+
+ FCB optl number of options
+
+optns EQU *
+ FCB DT.RBF RBF device
+ FCB $01 drive number
+ FCB $00 step rate
+ FCB $80 type=nonstd,coco
+ FCB $01 double density
+ FDB $005a tracks
+ FCB $40 one side
+ FCB $01 no verify
+ FDB $0040 sectors/track
+ FDB $0040 "", track 0
+ FCB $03 interleave
+ FCB $20 min allocation
+optl EQU *-optns
+
+rnam FCS /H0/
+fmnam FCS /RBF/
+drvnam FCS /EmuDsk/
+
+ EMOD
+rend EQU *
+ end
+
