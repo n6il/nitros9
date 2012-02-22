c54974c2c492a69f7d4e1402db5ff3327632e255
Boisy Pitre <coco@toughmac.com>
2012-02-22 06:38:54 -0600

    Atari port now boots

---
+++ defs/atarivtio.d
@@ -0,0 +1,36 @@
+               IFNE      VTIODEFS-1
+VTIODEFS       SET       1
+
+********************************************************************
+* VTIODefs - Video Terminal I/O Definitions for Atari XE/XL
+*
+* $Id$
+*
+* Edt/Rev  YYYY/MM/DD  Modified by
+* Comment
+* ------------------------------------------------------------------
+*          2012/02/21  Boisy G. Pitre
+* Started
+
+               NAM       VTIODefs  
+               TTL       Video Terminal I/O Definitions for Atari XE/XL
+
+               USE       antic.d
+               
+********************
+* VTIO Definitions
+*
+ScrStart       EQU       $0500
+Cols           EQU       40
+Rows           EQU       24
+
+********************
+* VTIO Static Memory
+*
+               ORG       V.SCF
+V.CurRow       RMB       1
+V.CurCol       RMB       1
+               RMB       250-.
+V.Last         EQU       .
+
+               ENDC      
