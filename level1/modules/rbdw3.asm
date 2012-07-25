e0e1bc97b0df4b71f1de0b79b978fa75727efc2e
Boisy Pitre <boisy.pitre@nuance.com>
2012-07-25 11:25:34 -0500

    Fixed stupid mistake in branch table

--- level1/modules/rbdw3.asm
+++ level1/modules/rbdw3.asm
@@ -61,8 +61,7 @@ name     fcs   /rbdw3/
 
 start    bra   Init
          nop
-         bra   Read
-         nop
+         lbra   Read
          lbra  Write
          lbra  GetStat
          lbra  SetStat
@@ -88,21 +87,11 @@ Term
 * Fix crash in certain cases
          beq   no@
          ldy   #$0001
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         ora   #DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          lda   #OP_TERM
          pshs a
          leax ,s
          orcc  #IntMasks
          jsr   DW$Write,u
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         anda  #^DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          clrb
          puls a
 no@      puls cc,pc
@@ -158,22 +147,12 @@ Init2    sta   DD.TOT,x			invalidate drive tables
          stu   >D.DWSubAddr
          ENDC
 * Initialize the low level device
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         ora   #DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          jsr   DW$Init,u
          lda   #OP_INIT
          sta   ,s
          leax  ,s
          ldy   #$0001
          jsr   DW$Write,u
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         anda  #^DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          clrb
 
 InitEx
@@ -233,11 +212,6 @@ ReadSect pshs  cc
          ldb   #E$Unit
          bra   ReadEr2
 Read1    sta   driveno,u
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         ora   #DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          lda   #OP_READEX		load A with READ opcode
          
 Read2
@@ -289,11 +263,6 @@ ReadEr2  lda   9,s
          ora   #Carry
          sta   9,s
 ReadEx   leas  5,s
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         anda  #^DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          puls  y,u
          puls  cc,pc
 
@@ -313,10 +282,6 @@ Write    lda   #NUMRETRIES
          sta   retries,u
          pshs  cc
          pshs  u,y,x,b,a,cc
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         ora   #DWIOSEMA
-         sta   D.ATARIFLAGS
          ENDC
 * Send out op code and 3 byte LSN
          lda   PD.DRV,y
@@ -375,11 +340,6 @@ WritEx2  lda   9,s
          ora   #Carry
          sta   9,s
 WritEx   leas  5,s
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         anda  #^DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          puls  y,u
          puls  cc,pc
  
@@ -414,10 +374,6 @@ SetStat  lda   #OP_SETSTA
 *
 GetStat  
          IFNE  atari
-         lda   D.ATARIFLAGS
-         ora   #DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          lda   #OP_GETSTA
          clrb				clear Carry
          pshs  cc			and push CC on stack
@@ -436,11 +392,6 @@ GetStat
          ENDC
          jsr   6,u
          leas  3,s
-         IFNE  atari
-         lda   D.ATARIFLAGS
-         anda  #^DWIOSEMA
-         sta   D.ATARIFLAGS
-         ENDC
          puls  cc,pc
 		 
          emod
