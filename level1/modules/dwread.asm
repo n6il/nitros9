66446ff267d11e1f90fe401a66ec1ab02fdaca3d
Boisy Pitre <boisy.pitre@nuance.com>
2013-01-23 14:54:29 -0600

    boot_dw now uses dwinit.asm. dwinit.asm updated for Arduino

--- level1/modules/dwread.asm
+++ level1/modules/dwread.asm
@@ -20,20 +20,19 @@
           IFNE ARDUINO
 * Note: this is an optimistic routine. It presumes that the server will always be there, and
 * has NO timeout fallback. It is also very short and quick.
-DWRead    clra                          ; clear Carry (no framing error)
+DWRead    clra                       ; clear Carry (no framing error)
           pshs   u,x,cc              ; preserve registers
           leau   ,x
           ldx    #$0000
-loop@     tst    $FF51                  ; check for CA1 bit (1=Arduino has byte ready)
-          bpl    loop@                  ; loop if not set
-          ldb    $FF50                  ; clear CA1 bit in status register
-          stb    ,u+                    ; save off acquired byte
-          abx                           ; update checksum
+loop@     tst    $FF51               ; check for CA1 bit (1=Arduino has byte ready)
+          bpl    loop@               ; loop if not set
+          ldb    $FF50               ; clear CA1 bit in status register
+          stb    ,u+                 ; save off acquired byte
+          abx                        ; update checksum
           leay   ,-y
           bne    loop@
-
-          leay      ,x                  ; return checksum in Y
-          puls      cc,x,u,pc        ; restore registers and return
+          leay   ,x                  ; return checksum in Y
+          puls   cc,x,u,pc           ; restore registers and return
 
           ELSE
 
