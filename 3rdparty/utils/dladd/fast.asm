1a6f70f6027e1a640e17df1c60c75e24d9411016
David Ladd <davidwladd@gmail.com>
2017-09-09 20:17:42 -0500

    Added a couple more utilities to my utils section as well as a README.md
    Added source for fast.asm and slow.asm
    
     The fast binary strobes address $FFD9 to enable 1.79MHz mode.
    This is useful for returning the system back to full speed mode after using
    the slow utility.
    
     The slow binary strobes address $FFD8 to enable 0.89MHz mode.
    This utility is handy for the cases of testing some older programs for timing
    that were from L1.

---
+++ 3rdparty/utils/dladd/fast.asm
@@ -0,0 +1,45 @@
+********************************************************************
+* fast - Set Fast mode of 1.79MHz
+*
+* $Id$
+*
+* Edt/Rev  YYYY/MM/DD  Modified by
+* Comment
+* ------------------------------------------------------------------
+*   1      2017/09/09  David Ladd
+* Started.
+*
+
+         nam   fast
+         ttl   Fast Mode
+
+         ifp1
+         use   defsfile
+         endc
+
+tylg     set   Prgrm+Objct
+atrv     set   ReEnt+Rev
+rev      set   $00
+edition  set   1
+
+         org   0
+         rmb   $0100      for the stack
+size     equ   .
+
+         mod   eom,name,tylg,atrv,start,size
+
+name     fcs   /fast/
+         fcb   edition
+
+clearn   fcb   C$FORM
+LCLEAR   equ   *-clearn
+
+* Entry of program
+Start    clrb
+         stb   >$FFD9
+         OS9   F$Exit
+
+         emod
+eom      equ   *
+         end
+
