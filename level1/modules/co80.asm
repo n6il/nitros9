8ab28a951d1ea48591246e8a192a22ad8b12415a
Tormod Volden <debian.tormod@gmail.com>
2018-08-29 09:05:17 +0200

    co80: Better description in source header

--- level1/modules/co80.asm
+++ level1/modules/co80.asm
@@ -1,10 +1,11 @@
 ********************************************************************
-* CO80 - Mysterious 80 Column Co-Driver for VTIO
+* CO80 - CRT9128 80 Column Co-Driver for VTIO
 *
 * $Id$
 *
-* This driver came with OS-9 Level One V2.00, yet it does not
-* seem to be written for the WordPak RS.
+* This driver came with OS-9 Level One V2.00, but is not
+* written for the 6845-based WordPak RS (I/II).
+* It works with a rare CRT9128-based PBJ WordPak prototype.
 *
 * Edt/Rev  YYYY/MM/DD  Modified by
 * Comment
